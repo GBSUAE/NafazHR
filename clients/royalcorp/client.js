9e4a868721f97b62e3c458eb04a8d32e3e82e7e1
GBSUAE <gapbridgings@gmail.com>
2025-06-14 00:51:34 +0400

    Royal Corp Onboarding

---
+++ clients/royalcorp/client.js
@@ -0,0 +1,6 @@
+document.title = "RoyalCorp HR Portal";
+
+const navbarTitle = document.querySelector('.navbar-title');
+if (navbarTitle) {
+  navbarTitle.textContent = "Welcome to RoyalCorp HR System";
+}
