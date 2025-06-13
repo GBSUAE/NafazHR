49076cdfc44e4b36ca6e339a68711c1c088d758d
GBSUAE <gapbridgings@gmail.com>
2025-06-14 00:22:38 +0400

    Add files via upload

---
+++ clients/demo/client.js
@@ -0,0 +1,6 @@
+document.title = "Demo HR Portal";
+
+const navbarTitle = document.querySelector('.navbar-title');
+if (navbarTitle) {
+  navbarTitle.textContent = "Welcome to Demo HR System";
+}
---
+++ clients/gbs123/client.js
@@ -0,0 +1,6 @@
+document.title = "GBS123 HR Portal";
+
+const navbarTitle = document.querySelector('.navbar-title');
+if (navbarTitle) {
+  navbarTitle.textContent = "Welcome to GBS123 HR System";
+}
