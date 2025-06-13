a6376eee832136d33f3727b628e13ee1d8ee867f
GBSUAE <gapbridgings@gmail.com>
2025-06-14 00:23:46 +0400

    Delete client directory

--- client/demo/client.js
+++
@@ -1,6 +0,0 @@
-document.title = "Demo HR Portal";
-
-const navbarTitle = document.querySelector('.navbar-title');
-if (navbarTitle) {
-  navbarTitle.textContent = "Welcome to Demo HR System";
-}
--- client/gbs123/client.js
+++
@@ -1,6 +0,0 @@
-document.title = "GBS123 HR Portal";
-
-const navbarTitle = document.querySelector('.navbar-title');
-if (navbarTitle) {
-  navbarTitle.textContent = "Welcome to GBS123 HR System";
-}
