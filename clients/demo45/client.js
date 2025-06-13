0f35b31f75448c4e3a931fecfe2f76b83dc5ef20
GBSUAE <gapbridgings@gmail.com>
2025-06-14 00:29:12 +0400

    Add files via upload

---
+++ clients/demo45/client.js
@@ -0,0 +1,6 @@
+document.title = "GBS123 HR Portal";
+
+const navbarTitle = document.querySelector('.navbar-title');
+if (navbarTitle) {
+  navbarTitle.textContent = "Welcome to DEMO45 HR System";
+}
